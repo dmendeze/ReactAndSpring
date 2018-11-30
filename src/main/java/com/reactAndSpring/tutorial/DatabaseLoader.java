53b4fddce1d6b57904e51cadc91bedc50f79f0ee
dmendeze <danielestrada36@gmail.com>
2018-11-29 21:10:44 -0500

    adding files for project

---
+++ src/main/java/com/reactAndSpring/tutorial/DatabaseLoader.java
@@ -0,0 +1,15 @@
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+	private final EmployeeRepository repository;
+
+	@Autowired
+	public DatabaseLoader(EmployeeRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+	}
+}
