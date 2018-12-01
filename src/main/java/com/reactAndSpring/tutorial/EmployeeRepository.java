3cedb4b086e8198e984e98b2a56e5b9afe0de486
dmendeze <danielestrada36@gmail.com>
2018-12-01 01:17:02 -0500

    build successful

--- src/main/java/com/reactAndSpring/tutorial/EmployeeRepository.java
+++ src/main/java/com/reactAndSpring/tutorial/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package com.reactAndSpring.tutorial;
 
+import com.reactAndSpring.tutorial.Employee;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
--- src/main/java/com/reactAndSpring/tutorial/HomeController.java
+++ src/main/java/com/reactAndSpring/tutorial/HomeController.java
@@ -7,4 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import tutorial.Employee;
+import com.reactAndSpring.tutorial.Employee;
+import com.reactAndSpring.tutorial.EmployeeRepository;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
