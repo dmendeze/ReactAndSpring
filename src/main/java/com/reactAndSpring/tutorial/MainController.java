f5e91259811dfff5bb55f77e1c4312c36f774890
dmendeze <danielestrada36@gmail.com>
2018-12-01 00:13:44 -0500

    migrating to intellij

---
+++ src/main/java/com/reactAndSpring/tutorial/MainController.java
@@ -0,0 +1,10 @@
+package com.reactAndSpring.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import tutorial.Employee;
